46a5a776bef14bf298881274c534c51217c7de0d
imfly <kubying@qq.com>
2015-11-02 23:19:51 +0800

    add .editorconfig

---
+++ test/index_test.js
@@ -0,0 +1,15 @@
+var should = require('should');
+describe('Index.js', function() {
+    describe('#filterCatelogFiles()', function() {
+        it('should get filtered catelog files when bookCatalog == `all`', function() {
+            return books.generate()
+                // parse('basic')
+                //   .then(function(book) {
+                //       return book.formatString('markdown', 'this is a **test**');
+                //   })
+                //   .then(function(content) {
+                //       content.should.equal('<p>this is a <strong>test</strong></p>\n');
+                //   });
+        });
+    });
+});
